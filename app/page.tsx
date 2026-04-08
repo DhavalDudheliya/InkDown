749c3b365fac2f0c312fca305bf134492baf7c45
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2026-04-09 00:03:41 +0530

    refactor: move the main page to root from /editor route

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import { redirect } from "next/navigation"
+import { Metadata } from "next"
+import { EditorPage } from "@/components/editor"
 
-export default function RootPage() {
-  redirect("/editor")
+export const metadata: Metadata = {
+  title: "InkDown",
+  description: "Convert your Markdown instantly to PDF with InkDown.",
+}
+
+export default function Page() {
+  return <EditorPage />
 }
